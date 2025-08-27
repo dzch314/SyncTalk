9caad55fc6dbd4d31e359f2019a7ac04e15f9ea9
Dmitry Chernyavsky <dzch314@gmail.com>
2025-08-27 23:13:03 +0300

    Add colors from Ella

---
+++ src/shared/lib/theme/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  primaryBgColor: '#d7cee1',
+  inverterPrimaryBgColor: '#8805de',
+  secondaryBgColor: '#2e054c',
+  primaryTextColor: '#000000',
+  secondaryTextColor: '#d7cee1',
+};
