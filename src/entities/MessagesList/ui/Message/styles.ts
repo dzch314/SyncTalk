a0c824acea2799d93bfe622f03e3c11dc47a388a
Dmitry Chernyavsky <dzch314@gmail.com>
2025-08-27 23:46:05 +0300

    Change styles for Message

--- src/entities/MessagesList/ui/Message/styles.ts
+++ src/entities/MessagesList/ui/Message/styles.ts
@@ -4,9 +4,9 @@ import { theme } from '@shared/lib/theme';
 
 export const styles = StyleSheet.create({
   messageContainer: { marginBottom: 16 },
-  name: { color: theme.primaryTextColor, marginBottom: 8 },
-  message: { backgroundColor: theme.inverterPrimaryBgColor, borderRadius: 16, padding: 12 },
+  name: { color: theme.primaryTextColor, marginBottom: 8, fontSize: 16, fontWeight: '500' },
+  message: { backgroundColor: theme.inverterPrimaryBgColor, borderRadius: 16, padding: 16 },
   activeMessage: { backgroundColor: theme.secondaryBgColor },
-  messageText: { color: theme.secondaryTextColor, fontSize: 16, fontWeight: '500' },
-  activeText: { fontWeight: '600' },
+  messageText: { color: theme.secondaryTextColor, fontSize: 20, fontWeight: '700' },
+  activeText: { fontWeight: '900' },
 });
